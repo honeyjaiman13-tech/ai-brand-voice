3d457ade5d97aee503004912948f538cc65495f6
honeyjaiman13-tech <honeyjaiman13@gmail.com>
2026-08-14 19:14:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if "history" not in st.session_state:
 with st.sidebar:
     st.header("Configuration")
     api_key = st.text_input("Gemini API Key:", type="password", help="Enter Key from Google AI Studio")
-    selected_model = st.selectbox("Select Model", ["gemini-2.5-flash", "gemini-1.5-pro"])
+    selected_model = st.selectbox("Select Model", ["gemini-2.0-flash", "gemini-1.5-pro"])
     st.divider()
     st.caption("Built for NASSCOM x Google Cloud Program")
 
